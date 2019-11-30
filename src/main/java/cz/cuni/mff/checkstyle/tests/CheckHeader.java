307d307adf801712205de7614bb7cead0e75be8e
Michal <mjombik@kkmbfin.sk>
2019-11-30 10:32:35 +0100

    fix header check

--- src/main/java/cz/cuni/mff/checkstyle/tests/CheckHeader.java
+++ src/main/java/cz/cuni/mff/checkstyle/tests/CheckHeader.java
@@ -1,11 +1,7 @@
 package cz.cuni.mff.checkstyle.tests;
 
-import cz.cuni.mff.checkstyle.utils.ContentReader;
-
 import java.io.IOException;
-import java.nio.file.FileSystems;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
@@ -16,19 +12,16 @@ public class CheckHeader implements Checker {
     public CheckHeader(String value) {
         try {
             header = Files.readAllLines(Paths.get(value));
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     @Override
     public void performCheck(List<String> lines, String relativePath) {
-        for (String line : header){
-            for (int i = 0; i < header.size(); i++) {
-                if (!header.get(i).equals(lines.get(i))) {
-                    System.out.println("iii: " + i);
-                    System.err.println(String.format("%s: Wrong header", relativePath));
-                }
+        for (int i = 0; i < header.size(); i++) {
+            if (!header.get(i).equals(lines.get(i))) {
+                System.err.println(String.format("%s: Wrong header", relativePath));
             }
         }
     }
