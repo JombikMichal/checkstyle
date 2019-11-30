e274b42d36b0fe5e7eb7c7fc3dccf58f21333819
Michal <mjombik@kkmbfin.sk>
2019-11-30 11:11:42 +0100

    remake getConfig function with lambdas

--- src/main/java/cz/cuni/mff/checkstyle/Main.java
+++ src/main/java/cz/cuni/mff/checkstyle/Main.java
@@ -1,7 +1,6 @@
 package cz.cuni.mff.checkstyle;
 
 import cz.cuni.mff.checkstyle.tests.Checker;
-import cz.cuni.mff.checkstyle.tests.FactoryCheck;
 import cz.cuni.mff.checkstyle.utils.ConfigReader;
 import cz.cuni.mff.checkstyle.utils.ContentReader;
 import cz.cuni.mff.checkstyle.utils.enums.FilesSuffix;
@@ -16,10 +15,16 @@ public  final class Main {
 
     public static void main(final String[] args) {
         try {
+            // args[0] means absolute path to particular directory
+            // get list of all files with .java suffix in current repository
             List<File> javaFiles = ContentReader.findFile(args[0],FilesSuffix.JAVA.toString());
+            // get all available checks from config file
             List<Checker> checks = ConfigReader.getConfig(args[0]);
+            // browse all java files and perform particular check on it via config
             for (File file : javaFiles){
+                // relative path must be set as parameter
                 String relativePath = ContentReader.getRelativePath(args[0],file);
+                // first and the most important parameter is content of file as list of strings
                 List<String> lines = Files.readAllLines(file.toPath());
                    for(Checker check : checks){
                        check.performCheck(lines,relativePath);
--- src/main/java/cz/cuni/mff/checkstyle/utils/ConfigReader.java
+++ src/main/java/cz/cuni/mff/checkstyle/utils/ConfigReader.java
@@ -5,22 +5,40 @@ import cz.cuni.mff.checkstyle.tests.FactoryCheck;
 
 import java.io.*;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.*;
 
 public class ConfigReader {
 
     private static final String CONFIG_FILE = "checkstyle.config";
 
-    public static List<Checker> getConfig(String base) throws IOException{
+    // TODO: remake this function with lambdas
+
+    public static List<Checker> getConfig(String base) throws IOException {
+        List<String> lines = Files.readAllLines(new File(base, CONFIG_FILE).toPath());
         List<Checker> checks = new ArrayList<>();
-        List<String> lines = Files.readAllLines(new File(base,CONFIG_FILE).toPath());
         FactoryCheck factoryCheck = new FactoryCheck(base);
-        for (String line : lines){
+        lines.forEach((l) -> {
+            String[] parts = l.split("=");
+            if (parts.length == 1) {
+                checks.add(factoryCheck.getCheck(parts[0], Optional.empty()));
+            } else if (parts.length == 2) {
+                checks.add(factoryCheck.getCheck(parts[0], Optional.of(parts[1])));
+            }
+        });
+        return checks;
+    }
+
+    // using simple for loop to find checks
+    public static List<Checker> getConfig2(String base) throws IOException {
+        getConfig2(base);
+        List<Checker> checks = new ArrayList<>();
+        List<String> lines = Files.readAllLines(new File(base, CONFIG_FILE).toPath());
+        FactoryCheck factoryCheck = new FactoryCheck(base);
+        for (String line : lines) {
             String[] parts = line.split("=");
-            if (parts.length == 1 ) {
-                checks.add(factoryCheck.getCheck(parts[0],Optional.empty()));
-            }else if (parts.length == 2){
+            if (parts.length == 1) {
+                checks.add(factoryCheck.getCheck(parts[0], Optional.empty()));
+            } else if (parts.length == 2) {
                 checks.add(factoryCheck.getCheck(parts[0], Optional.of(parts[1])));
             }
         }
